3ffe7ac5153e64b4b46c21a743aef0c40f84c8a9
Michael Toce <mptoce@gmail.com>
2023-09-07 21:21:40 -0400

    added button component and began coding Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,26 @@
+import Button from '../components/Button';
+import { arrowRight } from '../assets/icons';
+import { statistics } from '../constants';
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-coral-red font-palanquin text-xl">Our Summer Collections</p>
+        <h1 className="">
+          <span className="">The New Arrival</span>
+          <br />
+          <span>Nike</span>Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <Button label="Shop now" iconURL={arrowRight}/>
+
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {/* TODO: map over statistics  */}
+        </div>
+      </div>
+
+    </section>
   )
 }
 
