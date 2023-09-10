adb130d1838f8845d0b5db7ed9989704db659366
unknown <mptoce@gmail.com>
2023-09-10 18:34:35 -0400

    removed Hero border and fixed padding error within App.js

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
 
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-coral-red font-monsterrat text-xl">Our Summer Collections</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
