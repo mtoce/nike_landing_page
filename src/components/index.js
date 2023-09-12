44bb093fb8a5f1e3d3083bd3a10c75bf7049aecd
unknown <mptoce@gmail.com>
2023-09-12 12:55:43 -0400

    finished ServiceCard and Services section

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,11 @@ import Button from './Button';
 import ShoeCard from './ShoeCard';
 import Nav from './Nav';
 import PopularProductCard from './PopularProductCard';
+import ServiceCard from './ServiceCard';
 
-export { Button, ShoeCard, Nav, PopularProductCard }
+
+export { Button, 
+    ShoeCard, 
+    Nav, 
+    PopularProductCard, 
+    ServiceCard }
